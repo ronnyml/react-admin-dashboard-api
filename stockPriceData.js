2a00044ee2012176e698c2a79174b7cd9e51d9e3
Ronny Yabar <ronnycontacto@gmail.com>
2025-02-20 16:38:31 -0500

    Update month for testing data.

--- stockPriceData.js
+++ stockPriceData.js
@@ -1,19 +1,19 @@
 const stockPrices = {
   stock: "AAPL",
   prices: [
-    { date: "2025-01-01", open: 165.20, high: 170.50, low: 164.30, close: 168.90, volume: 12500000 },
-    { date: "2024-12-01", open: 162.80, high: 167.30, low: 161.50, close: 165.90, volume: 12000000 },
-    { date: "2024-11-01", open: 160.40, high: 165.00, low: 159.80, close: 162.50, volume: 11800000 },
-    { date: "2024-10-01", open: 157.20, high: 162.00, low: 156.50, close: 159.70, volume: 11500000 },
-    { date: "2024-09-01", open: 154.80, high: 159.60, low: 154.00, close: 157.20, volume: 11200000 },
-    { date: "2024-08-01", open: 152.50, high: 157.10, low: 151.80, close: 155.60, volume: 11000000 },
-    { date: "2024-07-01", open: 150.00, high: 154.90, low: 149.50, close: 152.80, volume: 10800000 },
-    { date: "2024-06-01", open: 147.80, high: 152.20, low: 147.00, close: 150.40, volume: 10600000 },
-    { date: "2024-05-01", open: 145.30, high: 149.70, low: 144.80, close: 147.90, volume: 10300000 },
-    { date: "2024-04-01", open: 142.90, high: 147.20, low: 142.00, close: 145.30, volume: 10000000 },
-    { date: "2024-03-01", open: 140.50, high: 144.80, low: 139.80, close: 142.90, volume: 9800000 },
-    { date: "2024-02-01", open: 138.20, high: 142.40, low: 137.50, close: 140.80, volume: 9600000 },
-    { date: "2024-01-01", open: 135.80, high: 140.00, low: 135.20, close: 138.50, volume: 9400000 }
+    { date: "Jan 2025", open: 180.30, high: 185.50, low: 175.20, close: 182.40, volume: 113250000 },
+    { date: "Dec 2024", open: 172.80, high: 176.90, low: 170.10, close: 173.40, volume: 112500000 },
+    { date: "Nov 2024", open: 166.20, high: 170.80, low: 164.00, close: 168.10, volume: 11850000 },
+    { date: "Oct 2024", open: 159.90, high: 165.40, low: 157.30, close: 162.70, volume: 111200000 },
+    { date: "Sep 2024", open: 155.20, high: 160.10, low: 150.90, close: 157.30, volume: 10850000 },
+    { date: "Aug 2024", open: 148.70, high: 153.60, low: 146.50, close: 151.80, volume: 10420000 },
+    { date: "Jul 2024", open: 143.80, high: 149.00, low: 141.30, close: 146.90, volume: 10100000 },
+    { date: "Jun 2024", open: 138.40, high: 144.20, low: 135.90, close: 141.10, volume: 19800000 },
+    { date: "May 2024", open: 134.90, high: 140.00, low: 130.50, close: 137.50, volume: 9600000 },
+    { date: "Apr 2024", open: 128.50, high: 135.70, low: 125.30, close: 132.80, volume: 9400000 },
+    { date: "Mar 2024", open: 120.90, high: 128.30, low: 118.20, close: 124.50, volume: 9200000 },
+    { date: "Feb 2024", open: 115.80, high: 123.40, low: 113.50, close: 120.90, volume: 9100000 },
+    { date: "Jan 2024", open: 108.50, high: 118.00, low: 105.60, close: 113.20, volume: 8900000 },
   ],
 };
 
